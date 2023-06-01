0075da14a1848e565383b502310bdbc131df8176
Daniel Flanagan <87383287+DanielFlanagan05@users.noreply.github.com>
2023-05-31 22:35:12 -0400

    Initial Commit

---
+++ GetUpAlarmClock/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GetUpAlarmClock
+//
+//  Created by Daniel Flanagan on 5/31/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GetUpAlarmClock/GetUpAlarmClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  GetUpAlarmClockApp.swift
+//  GetUpAlarmClock
+//
+//  Created by Daniel Flanagan on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct GetUpAlarmClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
